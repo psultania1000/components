793b0f1fcf01279a3f4a45f04a40e68cb4aca3a9
Priyansh Sultania <psultania1000@gmail.com>
2024-07-27 00:18:52 +0530

    making subheading dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+const reactDescriptions = ["Fundamentals", "Core", "Crucial"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 const Header = () => {
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
+        {reactDescriptions[genRandomInt(2)]} React concepts you will need for
+        almost any app you are going to build!
       </p>
     </header>
   );
